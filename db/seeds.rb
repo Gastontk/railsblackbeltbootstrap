24482e4bb9837f2b9b669e2ca14ca4449df611b5
Gaston Kennedy <gastontk@hotmail.com>
2016-10-18 14:37:09 -0700

    Cleanup forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+User.create(first_name:'snurg', last_name: 'stone', email:'snurgs@hotmail.com', password_digest: '$2a$10$dcOQKb51SJqZ4fdljo0gl.bZPkyTchG98N5Jzs9BsKG...')
